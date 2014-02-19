cd67bdbfe571683176edffcc128438c82615a64a
xiny0016 <txy443985164@gmail.com>
2014-02-19 11:18:19 +0100

    TCPEchoSetver

---
+++ EchoService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPServer
+{
+    public class EchoService
+    {
+        class doit()
+
+
+            while (true)
+            {
+                Socket connectionSocket = serverSocket.AcceptSocket();
+                Console.WriteLine("Server activated now");
+                EchoService service = new EchoService(connectionSocket);
+                service.doit();
+            }
+
+
+            Socket connectionSocket = serverSocket.AcceptSocket();
+            Console.WriteLine("Server activated");
+
+            Stream ns = new NetworkStream(connectionSocket);
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true; // enable automatic flushing
+
+            string message = sr.ReadLine();
+            string answer = "";
+            while (message != null && message != "")
+            {
+
+                Console.WriteLine("Client: " + message);
+                answer = message.ToUpper();
+                sw.WriteLine(answer);
+                message = sr.ReadLine();
+
+            }
+
+            ns.Close();
+            connectionSocket.Close();
+
+
+            serverSocket.Stop();
+
+        
+    }
+}
---
+++ TCPEchoServer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace TCPServer
+{
+    public class TCPEchoServer
+    {
+        public static void Main(string[] args)
+        {
+            TcpListener serverSocket = new TcpListener(6789);
+            serverSocket.Start();
+
+
+
+            while (true)
+            {
+                Socket connectionSocket = serverSocket.AcceptSocket();
+                Console.WriteLine("Server activated now");
+                EchoService service = new EchoService(connectionSocket);
+                service.doit();
+            }
+
+
+            Socket connectionSocket = serverSocket.AcceptSocket();
+            Console.WriteLine("Server activated");
+
+            Stream ns = new NetworkStream(connectionSocket);
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true; // enable automatic flushing
+
+            string message = sr.ReadLine();
+            string answer = "";
+            while (message != null && message != "")
+            {
+
+                Console.WriteLine("Client: " + message);
+                answer = message.ToUpper();
+                sw.WriteLine(answer);
+                message = sr.ReadLine();
+
+            }
+
+            ns.Close();
+            connectionSocket.Close();
+
+
+            serverSocket.Stop();
+
+        }
+    }
+}
